ebfc49a6a4a1440e11b08b62a92efe16b9a011b1
Juan_Montoya <juanesteban.montoya@utp.edu.co>
2023-01-14 23:37:29 -0500

    proyecto terminado

--- src/PaymentForm.js
+++ src/PaymentForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Cards from 'react-credit-cards';
 import 'react-credit-cards/es/styles-compiled.css';
 import TableOfPayment from "./TableOfPayment";
@@ -12,6 +12,27 @@ const PaymentForm = () => {
         cvc: '',
         focused: '',
     })
+    const [cardPosition, setCardPosition] = useState({
+        number1: "",
+        number2: "",
+        number3: "",
+        number4: ""
+    });
+    const [cards, setCards]=useState([]);
+
+
+    useEffect(() => {
+        let cards = JSON.parse(localStorage.getItem("cards"));
+        setCards(cards);
+    }, []);
+
+    const handleInputCard = () => {
+        setState({...state,number: cardPosition.number1 + cardPosition.number2 + cardPosition.number3 + cardPosition.number4});
+    }
+
+    useEffect(() => {
+        handleInputCard();
+    }, [cardPosition]);
 
     const handleInputChanges = (e) => {
         const {name, value} = e.target;
@@ -31,7 +52,6 @@ const PaymentForm = () => {
 
     const onChangeExpiry = (e) => {
         const {name, value} = e.target;
-        console.log(name, value);
         if (name === "month") {
             const year = state.expiry.split("/")[1];
             setState({
@@ -55,32 +75,47 @@ const PaymentForm = () => {
         })
     }
 
-    console.log(state.expiry);
 
     const processPayment = (e) => {
         e.preventDefault();
-        console.log("number => ", state.number);
-        console.log("name => ", state.name);
-        console.log("expiry => ", state.expiry);
-        console.log("cvc => ", state.cvc);
-        console.log(JSON.stringify(state));
         //Nombre >= 2 por que el nombre minimo requerido por algunas entidades bancarias es 2 caracteres
         if (state.number.length === 16 &&  state.name.length >= 2 && state.cvc.length === 3 && !state.expiry.includes("--")) {
             const {focused, cvc,...data} = state;
             let cards = JSON.parse(localStorage.getItem("cards"));
-            console.log(cards);
             if (cards === null) {
                 localStorage.setItem("cards", JSON.stringify([data]));
-                alert("Tarjeta guardada con exito");
                 setState({
                     number: '',
                     name: '',
                     expiry: '--/--',
                     cvc: '',
                     focused: '',
-                })
+                });
+                setCardPosition({
+                    number1: "",
+                    number2: "",
+                    number3: "",
+                    number4: ""
+                });
+                setCards([...cards , data]);
+                alert("Tarjeta guardada con exito");
                 return;
             }
+            setState({
+                number: '',
+                name: '',
+                expiry: '--/--',
+                cvc: '',
+                focused: '',
+
+            })
+            setCardPosition({
+                number1: "",
+                number2: "",
+                number3: "",
+                number4: ""
+            });
+            setCards([...cards , data]);
             localStorage.setItem("cards", JSON.stringify([...cards,data]));
             alert("Tarjeta guardada con exito");
         }
@@ -101,15 +136,16 @@ const PaymentForm = () => {
                 />
 
                 <form onSubmit={processPayment}>
-                    <div className="form-group">
+                    <div className="form-group mt-3">
                         <label htmlFor="number">Numero</label>
+                        <div className="d-flex flex-row">
                         <input
                             type="number"
                             name="number"
-                            value = {state.number}
-                            id="number"
-                            className="form-control"
-                            onChange={handleInputChanges}
+                            value = {cardPosition.number1}
+                            id="input1"
+                            className="form-control w-25 mr-3"
+                            onChange={(e)=>cardPosition.number2.length > 0 || cardPosition.number1.length > 4? document.getElementById("input2").focus():cardPosition.number1.length === 4? setCardPosition({...cardPosition, number2: e.target.value.slice(4,5)}) : setCardPosition({...cardPosition, number1: e.target.value})}
                             onFocus={handleFocusChange}
                             //Expresion regular que solo permite numeros
                             onKeyPress={event => {
@@ -119,8 +155,110 @@ const PaymentForm = () => {
                                     event.preventDefault();
                                 }
                             }}
+                            onKeyUp={event => {
+                                if(event.key === "Backspace" && cardPosition.number1.length === 4){
+                                    console.log("entros")
+                                    setCardPosition({...cardPosition, number1: cardPosition.number1.slice(0, -1)})
+                                    return
+                                }
+                                if (event.target.value.length >= 4) {
+                                    document.getElementById("input2").focus();
+                                }}
+                            }
 
                         />
+                        <input
+                            type="number"
+                            name="number"
+                            value = {cardPosition.number2}
+                            id="input2"
+                            className="form-control w-25 mr-3"
+                            onChange={(e)=>cardPosition.number3.length > 0 || cardPosition.number2.length > 4? document.getElementById("input3").focus():cardPosition.number2.length === 4? setCardPosition({...cardPosition, number3: e.target.value.slice(4,5)}) : setCardPosition({...cardPosition, number2: e.target.value})}
+                            onFocus={handleFocusChange}
+                            //Expresion regular que solo permite numeros
+                            onKeyPress={event => {
+                                const key = event.keyCode || event.which;
+                                const keyValue = String.fromCharCode(key);
+                                if(!/[0-9]/.test(keyValue)){
+                                    event.preventDefault();
+                                }
+                            }}
+                            onKeyUp={event => {
+                                if(event.key === "Backspace" && cardPosition.number2.length === 4){
+                                    console.log("entros")
+                                    setCardPosition({...cardPosition, number2: cardPosition.number2.slice(0, -1)})
+                                    return
+                                }
+                                if(event.key === "Backspace" && cardPosition.number2.length === 0){
+                                    document.getElementById("input1").focus();
+                                    return
+                                }
+                                if (event.target.value.length >= 4) {
+                                    document.getElementById("input3").focus();
+                                }}
+                            }
+
+                        />
+                        <input
+                            type="number"
+                            name="number"
+                            value = {cardPosition.number3}
+                            id="input3"
+                            className="form-control w-25 mr-3"
+                            onChange={(e)=>cardPosition.number4.length > 0 || cardPosition.number3.length > 4? document.getElementById("input4").focus():cardPosition.number3.length === 4? setCardPosition({...cardPosition, number4: e.target.value.slice(4,5)}) : setCardPosition({...cardPosition, number3: e.target.value})}
+                            onFocus={handleFocusChange}
+                            //Expresion regular que solo permite numeros
+                            onKeyPress={event => {
+                                const key = event.keyCode || event.which;
+                                const keyValue = String.fromCharCode(key);
+                                if(!/[0-9]/.test(keyValue)){
+                                    event.preventDefault();
+                                }
+                            }}
+                            onKeyUp={event => {
+                                if(event.key === "Backspace" && cardPosition.number3.length === 4){
+                                    console.log("entros")
+                                    setCardPosition({...cardPosition, number3: cardPosition.number3.slice(0, -1)})
+                                    return
+                                }
+                                if(event.key === "Backspace" && cardPosition.number3.length === 0){
+                                    document.getElementById("input2").focus();
+                                    return
+                                }
+                                if (event.target.value.length >= 4) {
+                                    document.getElementById("input4").focus();
+                                }}
+                            }
+
+                        />
+                        <input
+                            type="number"
+                            name="number"
+                            value = {cardPosition.number4}
+                            id="input4"
+                            className="form-control w-25"
+                            onChange={(e)=>cardPosition.number4.length >3?null:setCardPosition({...cardPosition, number4: e.target.value})}
+                            onFocus={handleFocusChange}
+                            //Expresion regular que solo permite numeros
+                            onKeyPress={event => {
+                                const key = event.keyCode || event.which;
+                                const keyValue = String.fromCharCode(key);
+                                if(!/[0-9]/.test(keyValue)){
+                                    event.preventDefault();
+                                }
+                            }}
+                            onKeyUp={event => {
+                                if(event.key === "Backspace" && cardPosition.number4.length === 4){
+                                    setCardPosition({...cardPosition, number4: cardPosition.number4.slice(0, -1)})
+                                }
+                                if(event.key === "Backspace" && cardPosition.number4.length === 0){
+                                    document.getElementById("input3").focus();
+                                }
+                            }
+                            }
+
+                        />
+                        </div>
                     </div>
 
                     <div className="form-group">
@@ -130,18 +268,20 @@ const PaymentForm = () => {
                             name="name"
                             id="name"
                             maxLength="30"
+                            value = {state.name}
                             className="form-control"
                             onChange={handleInputChanges}
                             onFocus={handleFocusChange}
                         />
                     </div>
 
-                    <div className="form-row g-2">
-                        <div className="from-group col-sm-6">
+                    <div className="g-2">
+                        <div className="from-group">
                             <label htmlFor="expity">Fecha de expiracion</label>
-                            <div className="form-group">
-                                <label htmlFor="month">Mes</label>
-                                <select className="form-control" id="month"  name="month" onChange={onChangeExpiry} onFocus={handleFocusChange}>
+                            <div className="d-flex flex-row">
+                            <div className="form-group w-50 mr-4">
+                                <select className="form-control" value={state.expiry.substring(0,2)} id="month"  name="month" onChange={onChangeExpiry} onFocus={handleFocusChange}>
+                                    <option value="--" disabled selected>Mes</option>
                                     <option value="01">Enero</option>
                                     <option value="02">Febrero</option>
                                     <option value="03">Marzo</option>
@@ -156,9 +296,9 @@ const PaymentForm = () => {
                                     <option value="12">Diciembre</option>
                                 </select>
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="year">Año</label>
-                                <select className="form-control" id="year" name="year"onChange={onChangeExpiry} onFocus={handleFocusChange}>
+                            <div className="form-group w-50">
+                                <select className="form-control" id="year" value={state.expiry.split("/")[1]} name="year"onChange={onChangeExpiry} onFocus={handleFocusChange}>
+                                    <option value="--" disabled selected>Año</option>
                                     <option value="22">2022</option>
                                     <option value="23">2023</option>
                                     <option value="24">2024</option>
@@ -175,8 +315,9 @@ const PaymentForm = () => {
                                     <option value="35">2035</option>
                                 </select>
                             </div>
+                            </div>
                         </div>
-                        <div className="from-group col-sm">
+                        <div className="from-group">
                             <label htmlFor="cvc">CVC</label>
                             <input
                                 type="number"
@@ -199,7 +340,7 @@ const PaymentForm = () => {
                     <button type="submit" className="btn btn-success btn-block btn-lg mt-3">Guardar</button>
                 </form>
             </div>
-            <TableOfPayment/>
+            <TableOfPayment cards={cards}/>
         </div>
     )
 
